c84530a7cbed66bdb869f4f877fbd0d05a90ceb8
Artur Sobczak <sobczak.artur88@gmail.com>
2023-01-07 23:00:48 +0100

    Try_Another_One

---
+++ src/com/sobczak/artur/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println("Proszę podać wysokość choinki?");
+        Scanner sc = new Scanner(System.in);
+        int columnNumber = sc.nextInt();
+
+        for (int i = 0; i < columnNumber; i++){
+            for (int j = 0; j <= columnNumber - i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= columnNumber - (columnNumber - i * 2); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
